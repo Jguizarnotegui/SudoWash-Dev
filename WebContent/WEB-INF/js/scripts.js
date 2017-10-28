098c93e1b4a2ca6a4c3b98aaa02608692f324def
Vrezh Khalatyan <vkhalat@calstatela.edu>
2017-10-28 15:47:41 -0700

    update on requestForm

--- WebContent/WEB-INF/js/scripts.js
+++ WebContent/WEB-INF/js/scripts.js
@@ -1,1 +1,22 @@
-
+//Calculate Total on Form
+function calculatePrice(getSudoWashForm){
+	//to get selected data
+	var elt = document.getElementById("colorItem");
+	var colors = elt.options[elt.selectedIndex].value;
+	
+	var elt = document.getElementById("blackItem");
+	var blacks = elt.options[elt.selectedIndex].value;
+	
+	var elt = document.getElementById("whiteItem");
+	var whites = elt.options[elt.selectedIndex].value;
+	
+	//converting data to integers
+	colors = parseInt(colors);
+	blacks = parseInt(blacks);
+	whites = parseInt(whites);
+	
+	//calculate the total value
+	var total = colors + blacks + whites;
+	
+	document.getElementById("ExitPrice").value=total;
+}
